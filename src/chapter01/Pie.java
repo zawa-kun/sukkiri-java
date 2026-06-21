81a7bc2046338b9eb2d797eaf0d4c3a525c2b58a
zawa-kun <shult.0219@gmail.com>
2026-06-21 22:57:28 +0900

    chapter1

---
+++ src/chapter01/Pie.java
@@ -0,0 +1,10 @@
+package chapter01;
+
+public class Pie {
+    public static void main(String... args) {
+        final double pi = 3.14;
+        /// pi = 3.14; // コンパイル時にエラーが出る
+
+        System.out.println(pi);
+    }
+}
---
+++ src/chapter01/Practice03.java
@@ -0,0 +1,17 @@
+package chapter01;
+
+public class Practice03 {
+    public static void main(String... args) {
+        // 1
+        boolean flag = true;
+        // 2
+        char c = '駆';
+        // 3
+        double pi = 3.14;
+        // 4
+        long bigNum = 3145926583979L;
+        // 5
+        String s = "ミナトの攻撃！敵に15ポイントのダメージを与えた。";
+    }
+
+}
